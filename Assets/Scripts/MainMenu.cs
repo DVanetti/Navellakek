ccdf38b8ba66d4b33eb143865e6ee3781bec8cba
Vanetti <Vanetti@mail>
2018-06-08 01:49:41 +0400

    TextMeshPro и Меню.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+[SerializeField] List<GameObject> windows;
+	public void PlayGame ( ) 
+                  {
+                         SceneManager.LoadScene(SceneManager.GetActiveScene( ) .buildIndex + 1); }
+
+public void OpenWindowByName(string name)
+{
+foreach (GameObject g in windows)
+{
+g.SetActive(g.transform.name == name);
+}
+}
+public void CloseWindowByName(string name)
+{
+foreach (GameObject g in windows)
+{
+if (g.transform.name == name) g.SetActive(false);
+}
+}
+public void StartGameplay()
+{
+SceneManager.LoadScene("Go", LoadSceneMode.Single);
+}
+}
+
+
